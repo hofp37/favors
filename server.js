ba583052cdf7d134c641c91caab490d1c6b74c4c
hofp37 <phoffmann@inwk.com>
2020-10-31 01:35:34 +0100

    nodejs express connected to react app

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const connectDB = require('./config/db')
+
+// load config
+dotenv.config({ path: './config/config.env' })
+
+connectDB()
+
+const app = express()
+
+// logging request etc to the console in dev env
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+// Routes
+app.use('/', require('./routes/index'))
+
+const PORT = process.env.PORT || 5000
+
+app.listen(
+    PORT,
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`)
+)
